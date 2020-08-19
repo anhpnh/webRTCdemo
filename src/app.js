b32b96c726972ee9e4b4d2bd0a2f306573d2a5d6
anhpnh <anhpnh@gmail.com>
2020-08-19 17:25:11 +0700

    B3 Answer peer sinh token-answer

--- src/app.js
+++ src/app.js
@@ -1,3 +1,18 @@
-console.log("Hello webpack");
 const openStream = require("./openStream");
-openStream();
+const $ = require("jquery");
+//openStream();
+const Peer = require("simple-peer");
+const p = new Peer({ initiator: location.hash === "#1", trickle: false });
+
+$(document).ready(function() {
+    console.log("Webpack loaded");
+    p.on("signal", token => {
+        $("#txtMySignal").val(JSON.stringify(token));
+    });
+
+    $("#btnConnect").click(() => {
+        const friendSignal = JSON.parse($("#txtFriendSignal").val());
+        p.signal(friendSignal);
+
+    });
+});
