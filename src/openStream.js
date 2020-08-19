0800b4d62c086b107dff1919325c1cee523f9fb4
anhpnh <anhpnh@gmail.com>
2020-08-19 18:05:08 +0700

    tach file function

--- src/openStream.js
+++ src/openStream.js
@@ -1,30 +1,10 @@
-const playVideo = require("./playVideo");
-const $ = require("jquery");
-const Peer = require("simple-peer");
-
-function openStream() {
+function openStream(cb) {
 
     navigator.mediaDevices.getUserMedia({ audio: true, video: true })
         .then(stream => {
-            playVideo(stream, 'localStream');
-
-            const p = new Peer({ initiator: location.hash === "#1", trickle: false, stream: stream });
-
-            p.on("signal", token => {
-                $("#txtMySignal").val(JSON.stringify(token));
-            });
-
-            $("#btnConnect").click(() => {
-                const friendSignal = JSON.parse($("#txtFriendSignal").val());
-                p.signal(friendSignal);
-
-            });
-
-            p.on('stream', friendStream => playVideo(friendStream, 'friendStream'))
+            cb(stream);
         })
-        .catch(err => console.log(err))
-
-
+        .catch(err => console.log(err));
 }
 
 module.exports = openStream;
